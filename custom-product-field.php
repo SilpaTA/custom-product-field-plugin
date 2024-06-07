96c49b79cc03084dea317040eaa7100bec88370a
Anandhu Nadesh <anandhu.natesh@gmail.com>
2024-06-07 17:54:38 +0530

    Initializing plugin

---
+++ custom-product-field.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Plugin Name: WooCommerce Custom Product Field
+ * Description: Adds a custom field to WooCommerce products and shows it on the product page, cart, and checkout.
+ * Version: 1.0
+ * Author: Silpa TA
+ * Author Url: https://github.com/SilpaTA/custom-product-field-plugin
+ */
+?>
